1594482fb87d8d46c9919bdf5d356f5778dc7be7
MiseikoDenis <68539464+MiseikoDenis@users.noreply.github.com>
2022-05-24 18:37:31 +0300

    Update Main.java

--- Lesson6/src/main/java/Main.java
+++ Lesson6/src/main/java/Main.java
@@ -4,12 +4,15 @@ public class Main {
         Person[] personRegistry = new Person[]{
                 new Person("Иван", 24, "Мужчина", "Беларусь", "Минск"),
                 new Person("Александр", 17, "Боевой вертолет Апачи", "Беларусь", "Брест"),
-                new Person("Инна",27,"Женщина","Беларусь","Минск"),
-                new Person("Петр",26,"Мужчина","Украина","Киев")
+                new Person("Инна", 27, "Женщина", "Беларусь", "Минск"),
+                new Person("Петр", 26, "Мужчина", "Украина", "Киев")
         };
 
         MilitaryOffice mf = new MilitaryOffice(personRegistry);
 
         mf.all();
+        readyCity("Минск");
+        readyAge();
+        readyName("Александр");
     }
 }
