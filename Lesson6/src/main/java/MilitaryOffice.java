2ac84defc553cfc135647ee8c8fa404eccc7acda
MiseikoDenis <68539464+MiseikoDenis@users.noreply.github.com>
2022-05-24 18:37:55 +0300

    Update MilitaryOffice.java

--- Lesson6/src/main/java/MilitaryOffice.java
+++ Lesson6/src/main/java/MilitaryOffice.java
@@ -11,9 +11,43 @@ public class MilitaryOffice {
             if (person.getGender().equals("Мужчина")
                     && person.getAge() < 27
                     && person.getAge() >= 18) {
-                System.out.println(person);
+                System.out.println(person.getName);
             }
         }
+    }
+
+    public void readyCity(String city) {
+        int count = 0;
+        System.out.print("Количество годных к службе в городе " + city + ": ");
+        for (Person person : personRegistry) {
+            if (person.getGender().equals("Мужчина")
+                    && person.getAge() < 27
+                    && person.getAge() >= 18
+                    && person.getCity.equals(city)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public void readyAge() {
+        System.out.print("Количество призывников в возрасте от 25 до 27: ");
+        for (Person person : personRegistry) {
+            if (person.getAge() < 27 && person.getAge() >= 25) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public void readyName(String name){
+        System.out.print("Количество призывников с именем " + name + ": ");
+        for (Person person : personRegistry) {
+            if (person.getName.equals(name)) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 
 
