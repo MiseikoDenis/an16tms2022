60437038b42a4b58d2a954e2bece2b739673f8e3
MiseikoDenis <den.miseiko@mail.ru>
2022-06-03 15:32:34 +0300

    first commit

---
+++ Lesson10/src/main/java/model/Car.java
@@ -0,0 +1,22 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Car {
+    private String model;
+    private int maxSpeed;
+    private int price;
+
+    public void start() throws EngineFailException {
+        if (((int) (Math.random() * 21)) % 2 != 0) throw new EngineFailException("Car " + model + " broke");
+        System.out.println("Car " + model + " started");
+    }
+
+}
---
+++ Lesson10/src/main/java/service/Main.java
@@ -0,0 +1,18 @@
+package service;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Car car1 = new Car("Mitsubishi", 250, 20000);
+//        Car car2 = new Car("Geely", 300, 50000);
+//        Car car3 = new Car("Reno", 200, 15000);
+//
+//        try {
+//            car1.start();
+//            car2.start();
+//            car3.start();
+//        } catch (EngineFailException e) {
+//            throw new RuntimeException(e);
+//        }
+    }
+}
