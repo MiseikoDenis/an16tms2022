66e01b81de6b88ec710e5bb84ea19ba88cc4b78e
MiseikoDenis <den.miseiko@mail.ru>
2022-06-06 16:27:52 +0300

    third commit

--- Lesson10/src/main/java/model/authorization/Authorization.java
+++ Lesson10/src/main/java/model/authorization/Authorization.java
@@ -2,16 +2,25 @@ package model.authorization;
 
 public class Authorization {
 
-    static boolean check;
+    private static boolean isAuthorized;
 
-    public static boolean authorization(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
-        if (login.length() > 20 || !login.matches("([a-zA-Z]*\\d*_*)*"))
-            throw new WrongLoginException("Login is incorrect!");
-        if (password.length() > 20
-                || !password.matches("([a-zA-Z]*\\d*_*)*")
-                || !password.equals(confirmPassword))
-            throw new WrongPasswordException("Password is incorrect!");
-        check = true;
-        return check;
+    public static boolean checkAuthorization(String login, String password, String confirmPassword) {
+        try {
+            if ((login.length() > 20
+                    || login.length() == 0
+                    || !login.matches("([a-zA-Z]*\\d*_*)*")))
+                throw new WrongLoginException("Login is incorrect!");
+            if (password.length() > 20
+                    || password.length() == 0
+                    || !password.matches("([a-zA-Z]*\\d*_*)*")
+                    || !password.equals(confirmPassword))
+                throw new WrongPasswordException("Password is incorrect!");
+            isAuthorized = true;
+
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+        }
+        return isAuthorized;
     }
+
 }
--- Lesson10/src/main/java/service/Main.java
+++ Lesson10/src/main/java/service/Main.java
@@ -1,8 +1,6 @@
 package service;
 
 import model.authorization.Authorization;
-import model.authorization.WrongLoginException;
-import model.authorization.WrongPasswordException;
 import model.car.Car;
 import model.car.EngineFailException;
 
@@ -10,24 +8,23 @@ public class Main {
     public static void main(String[] args) {
 
         //Задача с машиной
-        Car car1 = new Car("Mitsubishi", 250, 20000);
-        Car car2 = new Car("Geely", 300, 50000);
-        Car car3 = new Car("Reno", 200, 15000);
 
-        try {
-            car1.start();
-            car2.start();
-            car3.start();
-        } catch (EngineFailException e) {
-            throw new RuntimeException(e);
+        Car[] cars = new Car[]{
+                new Car("Mitsubishi", 250, 20000),
+                new Car("Geely", 300, 50000),
+                new Car("Reno", 200, 15000)
+        };
+
+        for (Car car : cars) {
+            try {
+                car.start();
+            } catch (EngineFailException e) {
+                System.out.println(e.getMessage());
+            }
         }
 
         //Задача про авторизацию
-        try {
-            boolean check = Authorization.authorization("_login34737asfa_236_", "pass1", "pass");
-            System.out.println(check);
-        } catch (WrongLoginException | WrongPasswordException e) {
-            throw new RuntimeException(e);
-        }
+        System.out.println("Authorization was complete? : "
+                + Authorization.checkAuthorization("_login34737asfa_236_", "pass", "pass"));
     }
 }
