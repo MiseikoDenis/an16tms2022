33f30622d87703cc2176021567b981251c42c82e
MiseikoDenis <den.miseiko@mail.ru>
2022-06-08 09:07:01 +0300

    third commit

--- Lesson10/src/main/java/model/authorization/Authorization.java
+++ Lesson10/src/main/java/model/authorization/Authorization.java
@@ -9,15 +9,10 @@ public class Authorization {
 
     public static boolean checkAuthorization(String login, String password, String confirmPassword) {
         try {
-            if (login.length() > 20
-                    || login.length() == 0
-                    || !login.matches("([a-zA-Z]*\\d*_*)*")) {
+            if (!login.matches("\\w{0,20}")) {
                 throw new WrongLoginException("Login is incorrect!");
             }
-            if (password.length() > 20
-                    || password.length() == 0
-                    || !password.matches("([a-zA-Z]*\\d*_*)*")
-                    || !password.equals(confirmPassword)) {
+            if (!password.matches("\\w{0,20}") || !password.equals(confirmPassword)) {
                 throw new WrongPasswordException("Password is incorrect!");
             }
             isAuthorized = true;
