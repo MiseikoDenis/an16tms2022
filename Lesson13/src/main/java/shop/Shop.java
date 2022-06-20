24aa7f377b4168eed3acdc6fffb5aad1b37dcb79
MiseikoDenis <den.miseiko@mail.ru>
2022-06-20 17:19:40 +0300

    first commit

--- Lesson13/src/main/java/shop/Shop.java
+++ Lesson13/src/main/java/shop/Shop.java
@@ -2,11 +2,123 @@ package shop;
 
 import lombok.AllArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 @AllArgsConstructor
 public class Shop {
     ArrayList<Product> list;
+
+    public void start() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("""
+                Выберите действие:
+                1. Показать все товары
+                2. Добавить новый товар
+                3. Удалить товар
+                4. Редактировать товар
+                5. Выход""");
+        int choice = in.nextInt();
+        switch (choice) {
+            case 1:
+                sort();
+                more();
+                break;
+            case 2:
+                addProduct();
+                more();
+                break;
+            case 3:
+                deleteProduct();
+                more();
+                break;
+            case 4:
+                setProduct();
+                more();
+                break;
+            case 5:
+                break;
+            default:
+                System.out.println("Неправильно выбран пункт. Повторите выбор");
+                start();
+        }
+        in.close();
+    }
+
+    public void sort() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("""
+                Выберите как список должен быть отсортирован
+                1. по цене (возрастанию)
+                2. по цене (убыванию)
+                3. по добавлению(сначала новые, потом более старые)""");
+        int choice = in.nextInt();
+        switch (choice) {
+            case 1 -> {
+                sortByPrice();
+                System.out.println(getList());
+            }
+
+            case 2 -> {
+                sortByPriceReversed();
+                System.out.println(getList());
+            }
+
+            case 3 -> {
+                sortByTime();
+                System.out.println(getList());
+            }
+
+            default -> {
+                System.out.println("Неправильно выбран пункт. Повторите выбор");
+                sort();
+            }
+        }
+    }
+
+    public void addProduct() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите id товара");
+        int id = in.nextInt();
+        System.out.println("Введите название товара");
+        String name = in.nextLine();
+        System.out.println("Введите цену товара");
+        int price = in.nextInt();
+        add(new Product(id, name, price, LocalDateTime.now()));
+    }
+
+    public void deleteProduct() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите id товара который хотите удалить");
+        int id = in.nextInt();
+        delete(id);
+    }
+
+    public void setProduct() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите id товара");
+        int id = in.nextInt();
+        System.out.println("Введите название товара");
+        String name = in.nextLine();
+        System.out.println("Введите цену товара");
+        int price = in.nextInt();
+        set(new Product(id, name, price, LocalDateTime.now()));
+    }
+
+    public void more() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("""
+                Вы хотите сделать что-то еще?
+                1. Да
+                2. Нет""");
+        int choice = in.nextInt();
+        switch (choice) {
+            case 1 -> start();
+            case 2 -> in.close();
+            default -> System.out.println("Неправильно выбран пункт. Повторите выбор");
+        }
+    }
 
     public void add(Product product) {
         if (list.isEmpty()) {
--- Lesson13/src/main/java/shop/Task5.java
+++ Lesson13/src/main/java/shop/Task5.java
@@ -9,21 +9,8 @@ public class Task5 {
         mall.add(new Product(24, "Milk", 20, LocalDateTime.now()));
         mall.add(new Product(25, "Cola", 15, LocalDateTime.now()));
         mall.add(new Product(28, "Soup", 23, LocalDateTime.now()));
-        System.out.println(mall.getList());
 
-        mall.delete(25);
-        System.out.println(mall.getList());
+        mall.start();
 
-        mall.set(new Product(24, "Cola", 25, LocalDateTime.now()));
-        System.out.println(mall.getList());
-
-        mall.sortByTime();
-        System.out.println(mall.getList());
-
-        mall.sortByPrice();
-        System.out.println(mall.getList());
-
-        mall.sortByPriceReversed();
-        System.out.println(mall.getList());
     }
 }
