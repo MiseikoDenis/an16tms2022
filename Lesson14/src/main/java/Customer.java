c56225c29b9262f6b4974f64c488e5873acd38d4
MiseikoDenis <den.miseiko@mail.ru>
2022-06-22 11:55:36 +0300

    first commit

--- Lesson14/src/main/java/Customer.java
+++ Lesson14/src/main/java/Customer.java
@@ -1,11 +1,25 @@
 import lombok.AllArgsConstructor;
 
+import java.util.Arrays;
+import java.util.concurrent.Semaphore;
+
 @AllArgsConstructor
-public class Customer implements Runnable {
+public class Customer extends Thread {
+    Semaphore sem;
+    int id;
     String[] purchases;
 
     @Override
     public void run() {
+        try {
+            sem.acquire();
+            System.out.println("Customer " + id + " come to the cash register with " + Arrays.toString(purchases));
+            sleep(1000);
+            System.out.println("Customer " + id + " finished shopping");
+            sem.release();
 
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- Lesson14/src/main/java/Main.java
+++ Lesson14/src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
+
 public class Main {
     public static void main(String[] args) {
+        Semaphore sem = new Semaphore(3);
+        ArrayList<Customer> queue = new ArrayList<>();
+        queue.add(new Customer(sem, 1, new String[]{"Milk", "Soup"}));
+        queue.add(new Customer(sem, 2, new String[]{"Cola"}));
+        queue.add(new Customer(sem, 3, new String[]{"Lay's", "Sprite"}));
+        queue.add(new Customer(sem, 4, new String[]{"Chicken", "Milk"}));
+        queue.add(new Customer(sem, 5, new String[]{"Sausages", "Pasta"}));
+        queue.add(new Customer(sem, 6, new String[]{"Pizza", "Cola"}));
+        queue.add(new Customer(sem, 7, new String[]{"Buckwheat", "Water"}));
+        queue.add(new Customer(sem, 8, new String[]{"Dumplings", "Ketchup"}));
 
+        for (Customer customer : queue) {
+            customer.start();
+        }
     }
 }
